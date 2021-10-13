9678582a688eedaf1f4c87f29dca7eecd3e84836
ALM21066-SANIAKAISER <87196750+ALM21066-SANIAKAISER@users.noreply.github.com>
2021-10-13 19:57:39 +0530

    Add files via upload

---
+++ ThreadDemo.java
@@ -0,0 +1,18 @@
+package Threads;
+
+public class ThreadDemo {
+	public static void main(String[] args)throws Exception {
+		Thread t=Thread.currentThread();
+		System.out.println(t);
+		t.setName("mythread...");
+		t.setPriority(10);
+		System.out.println(t);
+		
+		for(int i=0;i<10;i++) {
+			Thread.sleep(1000);
+			System.out.println(i);
+			System.exit(0);//this is used to terminate the main thread...
+		}
+	}
+}
+
